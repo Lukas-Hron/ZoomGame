17618ea8935bff68149212a29adc4cbd5abe28fe
Lukas Hron <lukasekhr@gmail.com>
2022-11-25 15:38:04 +0100

    fully functional

--- UnityGame/Zoom Game/Assets/Scripts/Gameplay/MouseVectorAnimationScrubber.cs
+++ UnityGame/Zoom Game/Assets/Scripts/Gameplay/MouseVectorAnimationScrubber.cs
@@ -4,21 +4,66 @@ using UnityEngine;
 
 public class MouseVectorAnimationScrubber : MonoBehaviour
 {
+    public Transform endPoint;
     private Animator anim;
     public string animClipName;
     [Range(0.0f, 0.99f)]
     public float normalizedTime = 0f;
+    bool isHolding;
+    Vector2 dragVector;
+    Vector2 projectedVector;
+    Vector3 startPoint;
 
-    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         anim.speed = 0;
     }
 
-    // Update is called once per frame
     void Update()
+    {  
+        if (isHolding)
+        {
+            anim.Play(animClipName, -1, normalizedTime);
+            if (Input.GetMouseButtonUp(0))
+            {
+                isHolding = false;
+                if (normalizedTime > 0.9) //Complete state
+                {
+                    normalizedTime = 0.99f;
+                    GetComponent<BoxCollider2D>().enabled = false;
+                    anim.Play(animClipName, -1, normalizedTime); //snap animation to right position
+                    return;
+                }
+                else //Failed to complete
+                {
+                    normalizedTime = 0f;
+                    anim.Play(animClipName, -1, normalizedTime); //snap animation to begining position
+                    return;
+                }
+            }
+
+            if (!Input.GetMouseButton(1) || !Input.GetMouseButton(3))
+            {
+                Vector2 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                projectedVector = Vector3.Project(currentMousePos, dragVector);
+                normalizedTime = InverseLerp(startPoint, endPoint.position, projectedVector);
+                normalizedTime = Mathf.Clamp(normalizedTime, 0f, 0.99f);
+            }
+        }
+    }
+
+    private void OnMouseDown()
     {
-        anim.Play(animClipName,-1,normalizedTime);
+        startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        isHolding = true;
+        dragVector = endPoint.position - startPoint;
+
+    }
+    public static float InverseLerp(Vector2 a, Vector2 b, Vector2 value) 
+    {
+        Vector2 AB = b - a;
+        Vector2 AV = value - a;
+        return Vector2.Dot(AV, AB) / Vector2.Dot(AB, AB);
     }
 }
