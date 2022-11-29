b4bdac93fb56fa55d13e1ffbc6974a3067805368
LucasKarjel <lucas.karjel@gmail.com>
2022-11-29 14:31:47 +0100

    Tiles for door

---
+++ UnityGame/Zoom Game/Assets/Scripts/Gameplay/GameBoard.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite tileOn;
+    public Sprite tileOff;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void ChangeSprite(Sprite tileOn, Sprite tileOff)
+    {
+            spriteRenderer.sprite = tileOn;
+            spriteRenderer.sprite = tileOff;
+    }
+ 
+    public void OnMouseDown()
+    {
+        //if (gameObject.name == "Tile1")
+            //if (spriteRenderer.sprite = tileOff)
+            //{
+            //    ChangeSprite();
+            //}
+            //else
+            //    ChangeSprite(tileOff);
+
+    }
+}
+
