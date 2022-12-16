278e0af9a2eadf0ac3feb1de98d8c185fd876689
sweviceroy <sweviceroy@gmail.com>
2022-12-16 11:34:01 +0100

    star constelation collider size increased

--- UnityGame/Zoom Game/Assets/Scripts/Gameplay/StarConstellation/ApateureRingScript.cs
+++ UnityGame/Zoom Game/Assets/Scripts/Gameplay/StarConstellation/ApateureRingScript.cs
@@ -8,12 +8,19 @@ public class ApateureRingScript : MonoBehaviour
     public bool sndRingDone;
     public bool thrdRingDone;
 
+    public GameObject path1;
+    public GameObject path2;
+    public GameObject path3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject apataturePart1;
+    public GameObject apataturePart2;
+    public GameObject apataturePart3;
+
+    public Sprite activatedPath;
+    public Sprite actiatedApaturePart;
+
+    public Animator apatureRingAnimation;
+
 
     // Update is called once per frame
     void Update()
@@ -26,14 +33,25 @@ public class ApateureRingScript : MonoBehaviour
         if (ringName == "fst")
         {
             fstRingDone = true;
+            path1.GetComponent<SpriteRenderer>().sprite = activatedPath;
+            apataturePart1.GetComponent<SpriteRenderer>().sprite = actiatedApaturePart;
         }
         else if (ringName == "snd")
         {
             sndRingDone = true;
+            path2.GetComponent<SpriteRenderer>().sprite = activatedPath;
+            apataturePart2.GetComponent<SpriteRenderer>().sprite = actiatedApaturePart;
         }
         else if (ringName == "thrd")
         {
             thrdRingDone = true;
+            path3.GetComponent<SpriteRenderer>().sprite = activatedPath;
+            apataturePart3.GetComponent<SpriteRenderer>().sprite = actiatedApaturePart;
+        }
+
+        if (fstRingDone && sndRingDone && thrdRingDone)
+        {
+            apatureRingAnimation.SetTrigger("win");
         }
     }
 }
