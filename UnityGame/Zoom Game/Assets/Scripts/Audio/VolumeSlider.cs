a9a3edb3521900650dbb2b615c6a79ff47ee9226
Lukas Hron <lukasekhr@gmail.com>
2022-12-20 15:36:23 +0100

    Revert "Merge branch 'dev' of https://github.com/Lukas-Hron/ZoomGame into dev"
    This reverts commit 985bc159a61261e698377aa16b843a1ab770fec1, reversing
    changes made to 2bdbe7bc348a02a933ce190b7b031a60537ed647.

--- UnityGame/Zoom Game/Assets/Scripts/Audio/VolumeSlider.cs
+++ UnityGame/Zoom Game/Assets/Scripts/Audio/VolumeSlider.cs
@@ -1,42 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
-    //[SerializeField] private Slider _slider;
-    //[SerializeField] private bool _musicVolume, _effectsVolume;
-    [SerializeField] AudioMixer _audioMixer;
+    [SerializeField] private Slider _slider;
+    [SerializeField] private bool _musicVolume, _effectsVolume;
 
-    //void Start()
-    //{
-    //    AudioHandler.Instance.ChangeEffectsVolume(_slider.value);
-    //    AudioHandler.Instance.ChangeMusicVolume(_slider.value);
+    void Start()
+    {
+        AudioHandler.Instance.ChangeEffectsVolume(_slider.value);
+        AudioHandler.Instance.ChangeMusicVolume(_slider.value);
 
-    //    if (_effectsVolume)
-    //        _slider.onValueChanged.AddListener(value => AudioHandler.Instance.ChangeEffectsVolume(value));
+        if (_effectsVolume)
+            _slider.onValueChanged.AddListener(value => AudioHandler.Instance.ChangeEffectsVolume(value));
         
-    //    if (_musicVolume)
-    //        _slider.onValueChanged.AddListener(value => AudioHandler.Instance.ChangeMusicVolume(value));
+        if (_musicVolume)
+            _slider.onValueChanged.AddListener(value => AudioHandler.Instance.ChangeMusicVolume(value));
         
-    //}
-    public void SetMasterVolume(float _sliderValue)
-    {
-        _audioMixer.SetFloat("MasterVolume", Mathf.Log10(_sliderValue) * 20);
-    }    
-    public void SetEffectsVolume(float _sliderValue)
-    {
-        _audioMixer.SetFloat("EffectsVolume", Mathf.Log10(_sliderValue) * 20);
-    }
-    public void SetMusicVolume(float _sliderValue)
-    {
-        _audioMixer.SetFloat("MusicVolume", Mathf.Log10(_sliderValue) * 20);
-    }
-    public void SetNarratorVolume(float _sliderValue)
-    {
-        _audioMixer.SetFloat("NarratorVolume", Mathf.Log10(_sliderValue) * 20);
     }
 }
 
