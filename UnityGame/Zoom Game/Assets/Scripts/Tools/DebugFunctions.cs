81725e78e9655d6548fe379e87f95feb2bf64b2c
Lukas Hron <lukasekhr@gmail.com>
2022-12-22 13:34:57 +0100

    drunk mode :D

--- UnityGame/Zoom Game/Assets/Scripts/Tools/DebugFunctions.cs
+++ UnityGame/Zoom Game/Assets/Scripts/Tools/DebugFunctions.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.Rendering.PostProcessing;
 
 public class DebugFunctions : MonoBehaviour
 {
+    public PostProcessVolume postpro;
+    private LensDistortion lens;
+    bool drunkmode;
     // Update is called once per frame
+
+    private void Start()
+    {
+        postpro.profile.TryGetSettings(out lens);
+    }
     void Update()
     {
         // Check if the "L" key is pressed
@@ -24,6 +33,24 @@ public class DebugFunctions : MonoBehaviour
             // Reload the current scene to restart the game
             SceneManager.LoadScene(0);
         }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            drunkmode = !drunkmode;
+
+            if (drunkmode)
+            {
+                lens.intensity.value = -100f;
+                lens.scale.value = 1.87f;
+            }
+            else
+            {
+                lens.intensity.value = -20f;
+                lens.scale.value = 1f;
+            }
+
+        }
+
     }
 }
 
